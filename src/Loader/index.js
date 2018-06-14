360c74c04637a78b992413a064bae59f27183daa
Pavel <psotnikov@deloitte.com>
2018-06-14 18:05:33 +0700

    Add global loader

--- src/Loader/index.js
+++ src/Loader/index.js
@@ -1,3 +1,18 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+import ReactDOM from 'react-dom'
+import './style.css'
 
-export default () => <h3>Loading data ...</h3>
+export default () => (
+  <Fragment>
+    {ReactDOM.createPortal(getLoader(), document.getElementById('loader'))}
+
+  </Fragment>
+)
+
+function getLoader() {
+  return (
+    <div className='loader-wrapper'>
+      <h1>Loading data ...</h1>
+    </div>
+  )
+}
